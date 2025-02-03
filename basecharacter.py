61b5bbac21d1f1131020fac921cc5035e5ef96ae
Niko Hunberg <arina.gunbina@yandex.ru>
2025-02-03 19:58:18 +0300

    added first version of a moving basic character and its tester

---
+++ basecharacter.py
@@ -0,0 +1,64 @@
+import pygame 
+
+class Guy:
+    dir = [-5, 5, 5, -5] # left, top, right, bottom
+    table = [['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '2', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'] ,
+                    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '0', '0', '0', '0', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '1', '0', '1', '1', '1', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '0', '0', '1', '0', '1', '1', '1', '0', '0', '0', '1', '1', '1', '1', '1', '0', '0', '0', '1', '1', '1', '0', '1', '0', '0', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '1', '1', '0', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '1', '1', '1', '0', '0', '1'] ,
+                    ['2', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '2'] ,
+                    ['1', '0', '0', '1', '1', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '1', '0', '1', '1', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '1', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '1', '1', '1', '0', '0', '0', '1', '0', '1', '0', '1', '1', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '1', '1', '1', '0', '0', '1', '1', '1', '1', '0', '0', '0', '0', '0', '1', '1', '1', '1', '0', '0', '1', '1', '1', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '1', '1', '1', '1', '0', '0', '0', '0', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '0', '0', '0', '1', '1', '1', '1', '0', '0', '1'] ,
+                    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'] ,
+                    ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '2', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
+    def __init__(self, board_size, cell_size, board_left, table=0):
+        self.obstacle = '1'
+        self.size = 10
+        self.curr_loc = (50, 50)
+        self.board_size = board_size
+        self.board_left = board_left
+        self.cell_size = cell_size
+        self.curr_dir = None
+
+    def get_cell(self, char_pos):
+        if char_pos[0] >= self.board_left[0] and char_pos[0] <= self.board_left[0] + self.cell_size * self.board_size[0] and char_pos[1] >= self.board_left[1] and char_pos[1] <= self.board_left[1] + self.cell_size * self.board_size[1]:
+            x = char_pos[0] - self.board_left[0]
+            y = char_pos[1] - self.board_left[1]
+            return (min(x // self.cell_size, self.board_size[0]-1), min(y // self.cell_size, self.board_size[1]-1))
+
+    def change_loc(self, dir):
+        self.curr_dir = dir
+        a, b = self.curr_loc
+        if dir == 0 or dir == 2:
+            next_loc = (min(max(0, a + self.dir[dir]), self.board_left[0]+self.board_size[0]*self.cell_size), b)
+            cell = self.get_cell(next_loc)
+            if self.table[cell[1]][cell[0]] == self.obstacle:
+                pass
+            else:
+                self.curr_loc = next_loc
+        elif dir == 1 or dir == 3:
+            next_loc = (a, min(max(0, b + self.dir[dir]), self.board_left[1]+self.board_size[1]*self.cell_size))
+            cell = self.get_cell(next_loc)
+            if self.table[cell[1]][cell[0]] == self.obstacle:
+                pass
+            else:
+                self.curr_loc = next_loc
+
+    def render_loc(self, screen):
+        pygame.draw.circle(screen, pygame.Color('yellow'), self.curr_loc, self.size)
+
+    def move(self, dir, screen):
+        self.change_loc(dir)
+        self.render_loc(screen)
+
+    def temp_board_render(self, screen):
+        for y in range(self.board_size[1]):
+            for x in range(self.board_size[0]):
+                if self.table[y][x] == self.obstacle:
+                    pygame.draw.rect(screen, pygame.Color('white'), pygame.Rect(self.board_left[0] + self.cell_size * x, self.board_left[1] + self.cell_size * y, self.cell_size, self.cell_size), 1)
---
+++ basecharactertester.py
@@ -0,0 +1,49 @@
+# pacman class is gonna be here
+import pygame
+from basecharacter import Guy
+
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+guy = Guy((31, 15), 30, (0, 0))
+guy.render_loc(screen)
+
+dirr = None
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    screen.fill("black")
+    guy.temp_board_render(screen)
+    action = 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.__dict__['key'] == pygame.K_UP:
+                guy.move(3, screen)
+                action = 0
+            elif event.__dict__['key'] == pygame.K_DOWN:
+                guy.move(1, screen)
+                action = 0
+            elif event.__dict__['key'] == pygame.K_RIGHT:
+                guy.move(2, screen)
+                action = 0
+            elif event.__dict__['key'] == pygame.K_LEFT:
+                guy.move(0, screen)
+                action = 0
+    if action:
+        guy.move(guy.curr_dir, screen)
+
+    # fill the screen with a color to wipe away anything from last frame
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
