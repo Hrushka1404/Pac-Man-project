d18895c88fc0b8f8bbda415f4d8cc7d138da85c2
me <irina.s.karpova@inbox.ru>
2025-03-01 20:06:45 +0300

    points eating

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
-# game starter
-# Example file showing a basic pygame "game loop"
-import pygame
+import pygame, sys
 from boardScript import Board
+from world import World
+from basecharacter import Guy
+from points import Point
 
-# pygame setup
 pygame.init()
+WIDTH, HEIGHT = 1280, 720
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+
+#game creation
 playboard = Board(screen)
+playboard.board_render(screen)
+guy = Guy((31, 15), 30, (0, 0))
+guy.render_loc(screen)
+points = Point((31, 15), 30, (0, 0))
+points.render(screen)
+
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    screen.fill("black")
+    action = 1
+    guy.temp_board_render(screen)
+    points.render(screen)
+    points.eat_point(guy.curr_loc)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
-
-    # RENDER YOUR GAME HERE
-
+        elif event.type == pygame.KEYDOWN:
+            if event.__dict__['key'] == pygame.K_UP:
+                guy.move(3, screen)
+                action = 0
+            elif event.__dict__['key'] == pygame.K_DOWN:
+                guy.move(1, screen)
+                action = 0
+            elif event.__dict__['key'] == pygame.K_RIGHT:
+                guy.move(2, screen)
+                action = 0
+            elif event.__dict__['key'] == pygame.K_LEFT:
+                guy.move(0, screen)
+                action = 0
+    if action:
+        guy.move(guy.curr_dir, screen)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
--- points.py
+++ points.py
@@ -16,20 +16,31 @@ class Point:
                     ['1', '0', '0', '1', '1', '1', '1', '0', '0', '0', '0', '1', '1', '1', '1', '0', '1', '1', '1', '1', '0', '0', '0', '0', '1', '1', '1', '1', '0', '0', '1'] ,
                     ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'] ,
                     ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '2', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
-    def __init__(self, board_size, cell_size, board_left, point_coord, table=0):
+    def __init__(self, board_size, cell_size, board_left, table=0):
         self.board_size = board_size
         self.cell_size = cell_size
         self.board_left = board_left
-        self.obstacle = 1
-        self.point_coord = point_coord
+        self.is_eaten = [[1 for j in range(board_size[0])] for i in range(board_size[1])]
 
 
     def render(self, screen):
-        for i in range(self.board_size[0]):
-            for j in range(self.board_size[1]):
-                if self.table[i][j] == '0' and self.obstacle == 1:
-                    curr_loc = (self.board_left[1] + j * self.cell_size, self.board_left[0] + i * self.cell_size)
+        for i in range(self.board_size[1]):
+            for j in range(self.board_size[0]):
+                if self.table[i][j] == '0' and self.is_eaten[i][j] == 1:
+                    curr_loc = (self.board_left[1] + (j) * self.cell_size + self.cell_size // 2, self.board_left[0] + (i) * self.cell_size + self.cell_size // 2)
                     pygame.draw.circle(screen, pygame.Color('white'), curr_loc, self.cell_size // 4)
+
+    def get_cell(self, char_pos):
+        if char_pos[0] >= self.board_left[0] and char_pos[0] <= self.board_left[0] + self.cell_size * self.board_size[0] and char_pos[1] >= self.board_left[1] and char_pos[1] <= self.board_left[1] + self.cell_size * self.board_size[1]:
+            x = char_pos[0] - self.board_left[0]
+            y = char_pos[1] - self.board_left[1]
+            return (int(min(x // self.cell_size, self.board_size[0]-1)), int(min(y // self.cell_size, self.board_size[1]-1)))
+
+    def eat_point(self, pac_coord):
+        x, y = self.get_cell(pac_coord)
+        if x > self.board_size[0] - 1 or y > self.board_size[1] - 1:
+            return
+        self.is_eaten[y][x] = 0
 
 
 
