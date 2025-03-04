5eae38825bb101b94b33ff9e1e49b56025bb63a3
Niko Hunberg <arina.gunbina@yandex.ru>
2025-03-04 20:07:19 +0300

    fixed inky behaviour

--- inkychar.py
+++ inkychar.py
@@ -7,6 +7,7 @@ class Inky(Blinky):
     def __init__(self, board_size, cell_size, board_left, table=0):
         super().__init__(board_size, cell_size, board_left, table)
         self.curr_loc = (27 * 30 + 15, 13 * 30 + 15)
+        self.board_edge = (self.board_left[0] + self.board_size[0] * self.cell_size, self.board_left[1] + self.board_size[1] * self.cell_size)
 
     def chase(self, pac_loc, blinky_loc, direc):
         m = 0
@@ -18,11 +19,10 @@ class Inky(Blinky):
         else:
             n = self.directions[direc]
         new_loc = (pac_loc[0] + m, pac_loc[1] + n)
-        final_loc = self.mirror(blinky_loc, new_loc)
-        if final_loc[0] >= 0 and final_loc[0] <= self.board_size[0] * self.cell_size and final_loc[1] >= 0 and final_loc[1] <= self.board_size[1] * self.cell_size:
-            self.find_targ(*final_loc)
-        else:
-            self.find_targ(*pac_loc)
+        mir_loc = self.mirror(blinky_loc, new_loc)
+        final_loc = (int(min(self.board_edge[0] - 1.5 * self.cell_size, max(self.board_left[0] + 1.5 * self.cell_size, mir_loc[0]))), int(min(self.board_edge[1] - 1.5 * self.cell_size, max(self.board_left[1] + 1.5 * self.cell_size, mir_loc[1]))))
+        # print(final_loc, self.board_edge)
+        self.find_targ(*final_loc)
 
     def mirror(self, loc, base_loc):
         x = 2 * base_loc[0] - loc[0]
